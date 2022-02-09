0c33f3c8077d13baa122c4613d85728c69c74126
Marko Šijačić <89016371+MarkoSijacic@users.noreply.github.com>
2022-02-09 02:29:12 +0100

    Create 15 Exercise 1-20.c

---
+++ Chapter 1/15 Exercise 1-20.c
@@ -0,0 +1,50 @@
+// @logos_design; K&R Book, Exercise 1-20
+
+// replace /t with enough space chars till the next tab stop (as per my understanding, tab stop is every 5 char columns)
+
+#include <stdio.h>
+#define TABSTOPLENGHT 8 //tabstop interval
+
+int detab(int current_count, int next_tabstop); //function definition
+
+int main (void)
+{
+    int c;
+    int count = 0; // number of input characters in a current line
+    int next_tab = TABSTOPLENGHT; // next tab stop variable
+
+    while ((c = getchar()) != EOF) // opens input line, classic getchar usage
+    {
+        if ((c != '\n') && (c != '\t')) // produces the correct output, updates count if no "Enter" or "Tab" inputs...
+        {
+            count++;
+            putchar(c);
+        }
+        else if (c == '\t') // if user enters TAB, this part of the code counts the next tab stop and calls detab function.
+        {
+            while (count >= next_tab)
+            {
+                next_tab += TABSTOPLENGHT;
+            }
+            count += detab(count, next_tab);
+        }
+        else if (c == '\n') // if enter is the input, resets count and next tab
+        {
+            putchar('\n');
+            putchar('\n');
+            count = 0;
+            next_tab = TABSTOPLENGHT;
+        }
+    }
+}
+
+int detab(int current_count, int next_tabstop) // self-explanatory, prints needed spaces instead of using regular tab. 
+{
+    int spaces = next_tabstop - current_count;
+
+    for (int i = spaces; i != 0; i--)
+    {
+        putchar(' ');
+    }
+    return spaces;
+}
